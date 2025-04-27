80e11e8690fd57917becc5f984615ed86215af9c
Ben Landrette <128928536+BendyLand@users.noreply.github.com>
2025-04-26 23:11:24 -0400

    Add readme and test files

---
+++ test/MainProgram.java
@@ -0,0 +1,6 @@
+public class MainProgram {
+    public static void main(String[] args) {
+        Student student = new Student("Alice", 20, "Computer Science");
+        student.introduce();
+    }
+}
